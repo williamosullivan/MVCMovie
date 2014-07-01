a842f6b0fb7307e40824765013d7d707becfb3cf
William O'Sullivan <williamosullivan@gmail.com>
2014-07-01 09:46:48 -0400

    FIxed Rating Not Saving
    Rating now displays and saves

--- MVCMovie/MVCMovie/Controllers/MoviesController.cs
+++ MVCMovie/MVCMovie/Controllers/MoviesController.cs
@@ -59,7 +59,7 @@ namespace MVCMovie.Controllers
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "ID,Title,ReleaseDate,Genre,Price")] Movie movie)
+        public ActionResult Create([Bind(Include = "ID,Title,ReleaseDate,Genre,Price,Rating")] Movie movie)
         {
             if (ModelState.IsValid)
             {
@@ -87,7 +87,7 @@ namespace MVCMovie.Controllers
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "ID,Title,ReleaseDate,Genre,Price")] Movie movie)
+        public ActionResult Edit([Bind(Include = "ID,Title,ReleaseDate,Genre,Price,Rating")] Movie movie)
         {
             if (ModelState.IsValid)
             {
